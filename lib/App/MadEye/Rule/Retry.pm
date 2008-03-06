d75942fe596bad954b9580f96ef243096529c93d
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-06 08:13:01 +0000

    リトライ機能をつけてやるぜ。
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@7564 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/App/MadEye/Rule/Retry.pm
@@ -0,0 +1,25 @@
+package App::MadEye::Rule::Retry;
+use strict;
+use warnings;
+use base qw/App::MadEye::Rule/;
+use Cache::FileCache;
+use YAML;
+
+sub dispatch {
+    my ($self, $context, $args) = @_;
+
+    my $expire_time = $self->config->{expire_time} or die "missing expire_time";
+    my $cache_root  = $self->config->{cache_root}  or die "missing cache_root";
+
+    my $key = YAML::Dump($args->{target});
+
+    my $cache = Cache::FileCache->new( { cache_root => $cache_root, } );
+    my $retry = $cache->get($key) ? 0 : 1;
+    $cache->set($key => "Boofy", $expire_time);
+
+    return $retry;
+}
+
+1;
+# HTTPD とかは頻繁に再起動するので、一回ぐらい落ちてても無視してほしい。
+
