df1cafcd365172da6525553958867d2624d899d0
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-06 01:47:34 +0000

    やっぱ Data::Dumper つかう
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@7531 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/MadEye/Plugin/Notify/Debug.pm
+++ lib/App/MadEye/Plugin/Notify/Debug.pm
@@ -2,12 +2,12 @@ package App::MadEye::Plugin::Notify::Debug;
 use strict;
 use warnings;
 use base qw/Class::Component::Plugin/;
-use YAML;
+use Data::Dumper;
 
 sub notify :Hook('notify') {
     my ($self, $context, $args) = @_;
 
-    warn Dump($args);
+    warn Dumper($args);
 }
 
 1;
