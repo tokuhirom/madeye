f825bf93f1ecd3a0e2385387fbf3be1cb45abeba
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-06 03:32:38 +0000

    added Agent::DNS
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@7542 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/App/MadEye/Plugin/Agent/DNS.pm
@@ -0,0 +1,33 @@
+package App::MadEye::Plugin::Agent::DNS;
+use strict;
+use warnings;
+use App::MadEye::Plugin::Agent::Base;
+use Net::DNS;
+
+sub is_dead {
+    my ($self, $target) = @_;
+
+    my $host = $target->{host} or die "missing host";
+    my $name = $target->{name} or die "missing name";
+    my $ip   = $target->{ip}   or die "missing ip";
+    my $timeout = $target->{timeout} || 10;
+
+    my $dns = Net::DNS::Resolver->new(
+        recurse     => 0,
+        nameservers => [$host],
+    );
+    $dns->tcp_timeout($timeout);
+    $dns->udp_timeout($timeout);
+    my $packet = $dns->search($name);
+    if ($packet) {
+        if ($packet->string =~ /$ip/) {
+            return; # alive.
+        } else {
+            return "invalid IP : " . $packet->string;
+        }
+    } else {
+        return 'DEAD';
+    }
+}
+
+1;
