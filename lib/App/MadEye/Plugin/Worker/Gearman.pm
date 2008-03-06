da72e044df17bfb2394c45f239739cebc1f988b3
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-06 06:10:41 +0000

    should_notify_p の中身は Filter となって不死鳥のごとくよみがえることだろうよ。
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@7558 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/MadEye/Plugin/Worker/Gearman.pm
+++ lib/App/MadEye/Plugin/Worker/Gearman.pm
@@ -13,7 +13,6 @@ use Storable qw/freeze thaw/;
 use YAML;
 
 our $TIMEOUT = 60;  # TODO: configurable
-our $EXPTIME = 180; # TODO: configurable
 
 sub run_workers : Method {
     my ($self, $context) = @_;
@@ -131,20 +130,11 @@ sub run_worker {
             my $result = \undef;
             timeout $TIMEOUT, "watching $args->{target} $args->{plugin}", sub {
                 if ( my $message = $args->{plugin}->is_dead( $args->{target} ) ) {
-                    # TODO: このあたりのルールもちゃんとつくる
-                    if (
-                        $self->should_notify_p(
-                            target  => $args->{target},
-                            context => $context,
-                        )
-                      )
-                    {
-                        $result = +{
-                            message => $message,
-                            plugin  => $args->{plugin},
-                            target  => $args->{target},
-                        };
-                    }
+                    $result = +{
+                        message => $message,
+                        plugin  => $args->{plugin},
+                        target  => $args->{target},
+                    };
                 }
             };
             return freeze($result);
@@ -157,49 +147,6 @@ sub run_worker {
         }
     );
     $worker->work while 1;
-}
-
-# これはここにあるべきか？ちげーだろ。
-sub should_notify_p {
-    my $self = shift;
-    validate(
-        @_ => {
-            target  => 1,
-            context => 1,
-        }
-    );
-    my %args = @_;
-    my $host = $args{host};
-
-    # ある時間帯だけスルーさせるルール
-    if ( my $param = $args{opt}->{args}->{neglect_hours}->{$host} ) {
-        my $now = DateTime->now;
-        my %ymd = map { ( $_ => $now->$_ ) } qw(year month day);
-        if (
-            $now->between(
-                map { DateTime->new( %ymd, %{ $param->{$_} } ) }
-                  qw(start_time end_time)
-            )
-          )
-        {
-            return 0;
-        }
-    }
-
-   # 再起動のタイミングとかで応答しないこともあるので、
-   # 一回ぐらい落ちてても気にしないというルール
-    if ( $args{opt}->{args}->{should_retry} ) {
-        my $cache_key = "$host-is-dead";
-
-        my $recent_dead_fg = $self->cache->get($cache_key) ? 1 : 0;
-
-        # 今回死んだことをここに記す。
-        $self->cache->set( $cache_key => 1, $EXPTIME );
-
-        return $recent_dead_fg;
-    }
-
-    return 1;    # 死んでるんだからおしらせしとけや
 }
 
 1;
