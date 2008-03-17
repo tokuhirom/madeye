bd160f9ac4098ddbf7e769130f5310f234fc1daf
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-17 03:18:34 +0000

    - Agent::Ping: added docs - Agent::Ping: added schema
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@8019 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/MadEye/Plugin/Agent/Ping.pm
+++ lib/App/MadEye/Plugin/Agent/Ping.pm
@@ -8,8 +8,7 @@ use Net::Ping;
 sub is_dead {
     my ($self, $host) = @_;
 
-    my $conf = $self->config->{config};
-    my $timeout = $conf->{timeout} || 5;
+    my $timeout = $self->config->{config}->{timeout};
 
     my $p = Net::Ping->new("tcp");
     $p->hires(1);
@@ -24,4 +23,30 @@ sub is_dead {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+App::MadEye::Plugin::Agent::Ping - ping! ping!
+
+=head1 SCHEMA
+
+    type: map
+    mapping:
+        target:
+            type: seq
+            required: yes
+            sequence:
+                - type: str
+        timeout:
+            required: yes
+            type: int
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno
+
+=head1 SEE ALSO
+
+L<App::MadEye>, L<Gearman::Client>, L<Gearman::Worker>
 
