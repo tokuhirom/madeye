7df446ffb764296efda6c18b2d3ba5aac5c2f46f
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-03-21 03:38:22 +0000

    added Agent::Sleep.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@8239 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/App/MadEye/Plugin/Agent/Sleep.pm
@@ -0,0 +1,49 @@
+package App::MadEye::Plugin::Agent::Sleep;
+use strict;
+use warnings;
+use App::MadEye::Plugin::Agent::Base;
+
+sub is_dead {
+    my ($self, $host) = @_;
+
+    my $conf = $self->config->{config};
+    my $sleep           = $conf->{sleep} or die "missing sleep";
+
+    sleep $sleep;
+
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::MadEye::Plugin::Agent::Sleep - everytime timeout
+
+=head1 DESCRIPTION
+
+THIS MODULE IS ONLY FOR DEBUGGING.
+
+=head1 SCHEMA
+
+    type: map
+    mapping:
+        target:
+            type: seq
+            required: yes
+            sequence:
+                - type: str
+        sleep:
+            required: yes
+            type: int
+
+=head1 AUTHORS
+
+Tokuhiro Matsuno
+
+=head1 SEE ALSO
+
+L<App::MadEye>
+
