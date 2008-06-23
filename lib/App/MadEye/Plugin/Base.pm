c74d851d5b690097bd9ffe650bfb725d1ca830fa
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-06-23 02:06:17 +0000

    show plugin name
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@14443 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/MadEye/Plugin/Base.pm
+++ lib/App/MadEye/Plugin/Base.pm
@@ -10,6 +10,7 @@ sub new {
     my $self = $class->SUPER::new(@_);
 
     if (my $schema = get_schema_from_pod($self)) {
+        local $SIG{__DIE__} = sub { die "$self: @_" };
         Kwalify::validate($schema, $self->config->{config});
     }
 
