deaac9c57f8d8c1f22133cc4164d371b4b635c2a
walf443 <walf443@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-11 01:24:42 +0000

    added Agent::Memcached.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-MadEye/trunk@26340 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/App/MadEye/Plugin/Agent/Memcached.pm
@@ -0,0 +1,63 @@
+package App::MadEye::Plugin::Agent::Memcached;
+use strict;
+use warnings;
+use Cache::Memcached::Fast;
+use App::MadEye::Plugin::Agent::Base;
+
+sub is_dead {
+    my ($self, $host) = @_;
+
+    my $conf    = $self->config->{config};
+    my $port    = $conf->{port} || 11211;
+    my $timeout = $conf->{timeout} || 1;
+    my $namespace => $conf->{namespace} || 'madeye';
+
+    my $sock = Cache::Memcached::Fast->new({
+        servers => ["$host:$port"],
+        namespace => $namespace,
+        connect_timeout => $timeout,
+    });
+
+    $sock->set($host, 1) or
+        return "Can't set data";
+    return "Can't get data" unless $sock->get($host);
+
+    return; # success!!
+}
+
+1;
+__END__
+
+=for stopwords Memcached
+
+=head1 NAME
+
+App::MadEye::Plugin::Agent::Memcached - check Memcached.
+
+=head1 SCHEMA
+
+    type: map
+    mapping:
+        target:
+            type: seq
+            required: yes
+            sequence:
+                - type: str
+        port:
+            required: no
+            type: int
+        timeout:
+            required: no
+            type: int
+        namespace:
+            required: no
+            type: str
+
+=head1 AUTHOR
+
+Keiji Yoshimi
+
+=head1 SEE ALSO
+
+L<App::MadEye>, L<Cache::Memcached::Fast>
+
