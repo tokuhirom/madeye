54ac591a52a8da1f454eeeda89fabbe71d480f5e
tokuhirom <tokuhirom@gmail.com>
2013-03-22 17:30:36 +0900

    fixed path

--- script/madeye.pl
+++ script/madeye.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 use File::Spec::Functions;
-use lib catfile($FindBin::Bin, 'lib');
+use lib catfile($FindBin::Bin, '..', 'lib');
 use App::MadEye;
 use Getopt::Long;
 
